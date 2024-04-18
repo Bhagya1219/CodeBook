1de40b6317accbe2f765ff88f4bdc14b7a93c0f7
Sruthi Tirumalasetty <thisissruthi12@gmail.com>
2024-04-18 03:34:07 +0100

    add navigation items to buttons

--- emart/src/component/Navbar.jsx
+++ emart/src/component/Navbar.jsx
@@ -28,11 +28,11 @@ const Navbar = () => {
                         </ul>
                         <div className='buttons'>
                             <button className='btn'>
-                                <NavLink to="" className="btn btn-outline-dark">
+                                <NavLink to="/login" className="btn btn-outline-dark">
                                     <i className="fa fa-sign-in me-1"></i>Login</NavLink>
-                                <NavLink to="" className="btn btn-outline-dark ms-2">
+                                <NavLink to="/register" className="btn btn-outline-dark ms-2">
                                     <i className="fa fa-user-plus me-1"></i>Register</NavLink>
-                                <NavLink to="" className="btn btn-outline-dark ms-2">
+                                <NavLink to="/cart" className="btn btn-outline-dark ms-2">
                                     <i className="fa fa-shopping-cart me-1"></i>Cart (0)</NavLink>
                             </button>
                         </div>

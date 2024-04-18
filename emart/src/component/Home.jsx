50ef959917597712afc61d9ed89c4d2fe0616bcc
Sruthi Tirumalasetty <thisissruthi12@gmail.com>
2024-04-18 02:45:24 +0100

    add filter ) wip not working

--- emart/src/component/Home.jsx
+++ emart/src/component/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Products from './Products';
 
 const Home = () => {
     return (
         <div className='hero'>
             <div class="card text-bg-dark text-black border-0">
-                <img src="/assets/bg.jpg" class="card-img" alt="Background"/>
+                <img src="/assets/bg.jpg" class="card-img" alt="Background" height="550px"/>
                     <div class="card-img-overlay d-flex flex-column justify-content-center">
                         <div className='container'>
                         <h5 class="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
@@ -12,7 +13,7 @@ const Home = () => {
                         </div>
                     </div>
             </div>
-
+            <Products/>
         </div>
     );
 }

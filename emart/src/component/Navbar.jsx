c04517a5a4948d0320c030808d85e197125dfc5e
Sruthi Tirumalasetty <thisissruthi12@gmail.com>
2024-04-18 01:36:19 +0100

    navigation bar template added

--- emart/src/component/Navbar.jsx
+++ emart/src/component/Navbar.jsx
@@ -4,38 +4,37 @@ const Navbar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                <div class="container">
+                    <a class="navbar-brand fw-bold fs-4" href="#">Bhagya Store</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="#">Products</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><hr class="dropdown-divider" /></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                <a class="nav-link" href="#">Contact</a>
                             </li>
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        <div className='buttons'>
+                            <button className='btn'>
+                                <a href="" className="btn btn-outline-dark">
+                                    <i className="fa fa-sign-in me-1"></i>Login</a>
+                                <a href="" className="btn btn-outline-dark ms-2">
+                                    <i className="fa fa-user-plus me-1"></i>Register</a>
+                                <a href="" className="btn btn-outline-dark ms-2">
+                                    <i className="fa fa-shopping-cart me-1"></i>Cart (0)</a>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </nav>
